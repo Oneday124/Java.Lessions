dccf9653049873208d0a92401e1263d4b695b930
Nikolay <Onedaydejavu124@mail.ru>
2023-06-25 19:31:44 +0700

    Опертор выбора switch

---
+++ selectionOperator.java
@@ -0,0 +1,19 @@
+// import java.util.Scanner;
+
+public class selectionOperator {
+    public static void main(String[] args) {
+        int mounth = 1;
+        String text = "";
+        switch (mounth) {
+            case 1:
+                text = "Autumn";
+                break;
+            default:
+                text = "mistake";
+                break;
+        }
+        System.out.println(text);
+        // iScanner.close();
+    }
+
+}
