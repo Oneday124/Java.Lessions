901a96464436e924139da59a9d8ffa1f3457e571
Nikolay <Onedaydejavu124@mail.ru>
2023-06-25 17:10:51 +0700

    Работа с терминалом 2

--- workTerminal.java
+++ workTerminal.java
@@ -1,10 +1,13 @@
 import java.util.Scanner;
+
 public class workTerminal {
     public static void main(String[] args) {
         Scanner iScanner = new Scanner(System.in);
-        System.out.printf("name: ");
-        String name = iScanner.nextLine();
-        System.out.printf("Привет, %s!", name);
+        System.out.printf("int a: ");
+        int x = iScanner.nextInt();
+        System.out.printf("Double a: ");
+        double y = iScanner.nextDouble();
+        System.out.printf("%d + %f = %f", x, y, x + y);
         iScanner.close();
     }
 }
