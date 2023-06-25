76174955d2f73d588f791ae34326b342c1212e16
Nikolay <Onedaydejavu124@mail.ru>
2023-06-25 19:06:27 +0700

    Методы и функции. Библиотека методов

---
+++ lib.java
@@ -0,0 +1,15 @@
+public class lib {
+    static void sayHi() {
+        System.out.println("Hi");
+    }
+
+    static int sum(int a, int b) {
+        return a + b;
+    }
+
+    static double factor(int n) {
+        if (n == 1)
+            return 1;
+        return n * factor(n - 1);
+    }
+}
