0e38e8f3524bdc6a6a378481a8d02f7b58dd6928
Nikolay <Onedaydejavu124@mail.ru>
2023-06-25 20:28:33 +0700

    Работа с файлами. Посимвольное чтение

---
+++ fileReadSymbol.java
@@ -0,0 +1,18 @@
+
+// Работа с файлами, чтение по символьно
+import java.io.*;
+
+public class fileReadSymbol {
+    public static void main(String[] args) throws Exception {
+        FileReader fr = new FileReader("file.txt");
+        int c;
+        while ((c = fr.read()) != -1) {
+            char ch = (char) c;
+            if (ch == '\n') {
+                System.out.print(ch);
+            } else {
+                System.out.print(ch);
+            }
+        }
+    }
+}
