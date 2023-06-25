cf6cdee6089b9665dbe7fb7c46a823b29ba8186d
Nikolay <Onedaydejavu124@mail.ru>
2023-06-25 19:05:34 +0700

    Методы и функции. Основная программа

---
+++ fonctionsAndMetods.java
@@ -0,0 +1,7 @@
+public class fonctionsAndMetods {
+    public static void main(String[] args) {
+        lib.sayHi(); // Hi
+        System.out.println(lib.sum(1, 3)); // 4
+        System.out.println(lib.factor(5)); // 120.0
+    }
+}
